0c3e74410cfc79b0724e24f1da2d34789301f332
Fredmaster08 <frxdmaster@github.com>
2025-08-30 19:41:06 +0200

    bug fixes

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct {
     unsigned int row;
@@ -27,6 +28,25 @@ void drawMap(char map[3][3]) {
         }
     }
 }
+                                                                                                                                   // well, its not going well
+bool wonMatch(char sym, char map[3][3]) {
+    if(map[2][0] ==  sym && map[1][1] == sym && map[0][2] == sym) {
+        return true;
+    }
+    if(map[2][2] ==  sym && map[1][1] == sym && map[0][0] == sym) {
+        return true;
+    }
+    for(int i = 0; i < 3; i++) {
+        if(map[i][0] == sym && map[i][1] == sym && map[i][2] == sym) {
+            return true;
+        }
+        if(map[0][i] == sym && map[1][i] == sym && map[2][i] == sym) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main() {
 
@@ -34,20 +54,38 @@ int main() {
 
     int userInput = 0;
 
-    printf("Enter a number between 1-9 to select your turn: ");
-    scanf("%d", &userInput);
-
-    Index index = getIndex(userInput);
-
     char map[3][3];
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
             map[i][j] = ' ';
         }
     }
-    map[index.row][index.column] = 'X';
+    
+    while(1) {
+        printf("Enter a number between 1-9 to select your turn: ");
+        scanf("%d", &userInput);
+        if(userInput > 9 || userInput < 1) {
+            printf("Out of bounds!\n");
+            continue;
+        }
 
-    drawMap(map);
+        Index index = getIndex(userInput);
+
+        if(map[index.row][index.column] != ' ') {
+            printf("Invalid input\n");
+            continue;
+        }
+
+        map[index.row][index.column] = 'X';
+
+        drawMap(map);
+
+        if(wonMatch('X', map)) {
+            printf("You won!\n");
+            break;
+        }
+    }
+
     
     return 0;
 }
